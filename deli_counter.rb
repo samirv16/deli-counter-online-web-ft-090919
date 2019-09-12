8d1689aecec947ee75b64867799d9823b2892074
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-12 01:56:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -30,7 +30,7 @@ end
 def now_serving(katz_deli)
   
   if katz_deli == 0 
-    puts "there is nobody in line"
+    puts "There is nobody waiting to be served"
     
   else
     puts "Currently serving #{katz_deli.shift}."
