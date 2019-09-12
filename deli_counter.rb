8ce87263283abab6c34a708cf232c7f7ddef9e0a
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-12 01:02:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,10 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    message="The line is currently:#{}"
+    message="The line is currently:"
 
     katz_deli.each_with_index do |value, index|
+       new_index = index + 1
        
     end
   end
