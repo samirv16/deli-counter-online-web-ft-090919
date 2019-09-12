4b31ddb4ff1df9ca1ebe81f51644cc3b5fb9ad48
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-12 00:56:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    message="The line is currently:"
+
+    katz_deli.each_with_index do |value, index|
+      queue += " #{index.to_i+1}. #{value}"
+    end
+  end
+    
+  end
